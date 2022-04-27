dca8d2947668d4f622e0011e16f364257b33b5d3
gcwillis14 <30315801+gcwillis14@users.noreply.github.com>
2022-04-27 17:07:27 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -1,49 +1,87 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    titlePanel("Cyber Bistro Championship of Cricket (CBCOC)"),
+    
+    # Create a new Row in the UI for selectInputs
+    fluidRow(
+        column(4,
+               selectInput("week",
+                           "Gameweek:",
+                           c("All",
+                             unique(as.character(data$Week))))
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        column(4,
+               selectInput("player",
+                           "Players:",
+                           c("All",
+                             unique(as.character(data$`The Vicars of Sibley`))))
         )
-    )
+    ),
+    # Create a new row for the table.
+    DT::dataTableOutput("table")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    
+    library(rvest)
+    library(Hmisc)
+    library(tidyverse)
+    
+    # get data
+    people <- c("12719","12929","12713","12712","12891") #list of IDs
+    
+    # Week 1
+    Weekly_total1 <- data.frame(matrix(1, ncol=1, nrow=13)) #create empty table
+    for (i in people){
+        content <- read_html(paste0("https://cricketxi.com/county-championship-2022/league/2859/team/", i , "/1/"))
+        tables <- content %>% html_table(fill = TRUE)
+        table <- tables[[1]]
+        table <- table[c(1,4,5,6,7,9,10,12,14,15,16,17,2),]
+        Weekly_total1 <- cbind(Weekly_total1, table)
+        names(Weekly_total1)[names(Weekly_total1) == 'matrix.1..ncol...1..nrow...13.'] <- 'Week'
+    }
+    
+    # Week 2
+    Weekly_total2 <- data.frame(matrix(2, ncol=1, nrow=13)) #create empty table
+    for (i in people){
+        content <- read_html(paste0("https://cricketxi.com/county-championship-2022/league/2859/team/", i , "/2/"))
+        tables <- content %>% html_table(fill = TRUE)
+        table <- tables[[1]]
+        table <- table[c(1,4,5,6,7,9,10,12,14,15,16,17,2),]
+        Weekly_total2 <- cbind(Weekly_total2, table)
+        names(Weekly_total2)[names(Weekly_total2) == 'matrix.2..ncol...1..nrow...13.'] <- 'Week'
+    }
+    
+    # Week 3
+    Weekly_total3 <- data.frame(matrix(3, ncol=1, nrow=13)) #create empty table
+    for (i in people){
+        content <- read_html(paste0("https://cricketxi.com/county-championship-2022/league/2859/team/", i , "/3/"))
+        tables <- content %>% html_table(fill = TRUE)
+        table <- tables[[1]]
+        table <- table[c(1,4,5,6,7,9,10,12,14,15,16,17,2),]
+        Weekly_total3 <- cbind(Weekly_total3, table)
+        names(Weekly_total3)[names(Weekly_total3) == 'matrix.3..ncol...1..nrow...13.'] <- 'Week'
+    }
+    
+    Total <- rbind(Weekly_total1, Weekly_total2, Weekly_total3)
+    data <- Total
+    
+    # Filter data based on selections
+    output$table <- DT::renderDataTable(DT::datatable({
+        data <- Total
+        if (input$week != "All") {
+            data <- data[data$Week == input$week,]
+        }
+        if (input$player != "All") {
+            data <- data[data$`The Vicars of Sibley` == input$player,]
+        }
+        data
+    }))
+    
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
